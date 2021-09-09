e5b352fb6171c5a2cbe891d3094024e2ba686b30
SJ kim <88877426+seonmind@users.noreply.github.com>
2021-09-09 19:34:17 +0900

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        global list
+        list=nums
+        length=len(list)
+        for i in range(0,length):
+            if list[i]=='_':
+                return i
+            for j in range(i,length):
+                if list[j]==list[i]:
+                    j+=1
+                else:
+                    break
+            for k in range(i+1,length-j+i+1):
+                list[k]=list[k+j-i-1]
+            for p in range(length-j+i+1,length):
+                list[p]='_'
