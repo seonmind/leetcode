95798911cbb40bc21d30b22b6b469c9024d7e6b6
SJ kim <88877426+seonmind@users.noreply.github.com>
2021-09-09 19:36:31 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,4 +1,5 @@
 #Remove Duplicates from Sorted Array
+#https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
